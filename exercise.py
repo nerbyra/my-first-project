4e55f00f3de45fa36eb8c561d3f3d4fca162dac0
nerbyra <nantandanabiirah@gmail.com>
2023-08-22 17:26:59 +0300

    test

---
+++ exercise.py
@@ -0,0 +1,10 @@
+def sum(timestamps):#
+tt_secs=0 #declaring and initialising
+for timestamps in timestamps #loop
+tt_sec+=minutes*60+tt_secs
+tt_mins, remainder_seconds = divmod(tt_secs,60)
+
+
+result _timestamp= f"{tt_mins:02}:{remainder_seconds:02}"
+
+return result
---
+++ main.py
@@ -0,0 +1,23 @@
+name='raihan'
+age = 45
+gender = 'F'
+marks = 48.9
+
+print(name)
+print(age)
+print(gender)
+print(marks)
+
+
+student = ['sarah',100,'f',46]
+
+print(student[2])
+print(student[0])
+print(student[1])
+
+def sum_time(time1,time2):
+    print(time1)
+    print('hello world')
+    return time2+time1
+    
+    print(sum_time(10.45, 22))
